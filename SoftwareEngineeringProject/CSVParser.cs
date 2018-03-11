b456343ce79f1e8d59131743c4912b54ce3592ca
Seanboyy <sbamford12@gmail.com>
2018-03-11 15:51:51 -0400

    started work on CSV parsing

---
+++ SoftwareEngineeringProject/CSVParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineeringProject
+{
+    class CSVParser
+    {
+        public static Class[] ParseCSV()
+        {
+            string[] lines = System.IO.File.ReadAllLines("Course List (CSV).csv");
+            string[] lines2 = System.IO.File.ReadAllLines("Prereqs List (CSV).csv");
+            int lineCount = lines.Length;
+            int lineCount2 = lines2.Length;
+            Class[] classes = new Class[lineCount];
+            for(int i = 0; i < lineCount - 1; ++i)
+            {
+                string[] tokens = lines[i + 1].Split(',');
+                classes[i] = new Class(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6], tokens[7], Convert.ToInt32(tokens[8]), Convert.ToInt32(tokens[9]));
+            }
+            for(int i = 0; i < lineCount2 - 1; ++i)
+            {
+                string[] tokens = lines2[i + 1]
+            }
+            return classes;
+        }
+    }
+}
---
+++ SoftwareEngineeringProject/Class.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineeringProject
+{
+    class Class
+    {
+        public string courseCode;
+        public string shortTitle;
+        public string longTitle;
+        public string beginTime;
+        public string endTime;
+        public string meets;
+        public string building;
+        public string room;
+        public Class[] prerequisites;
+        public int enrollment;
+        public int capacity;
+
+        public Class(string code, string shortTitle, string longTitle, string begin, string end, string days, string building, string room, int enroll, int capacity)
+        {
+            courseCode = code;
+            this.shortTitle = shortTitle;
+            this.longTitle = longTitle;
+            beginTime = begin;
+            endTime = end;
+            meets = days;
+            this.building = building;
+            this.room = room;
+            enrollment = enroll;
+            this.capacity = capacity;
+        }
+    }
+}
