1517d03fe1e97635e4d4473298a453156ff0f25d
GCC\MILLERSL1 <millersl1@gcc.edu>
2018-03-12 16:43:12 -0400

    Started work on UI

--- SoftwareEngineeringProject/Form1.cs
+++ SoftwareEngineeringProject/Form1.cs
@@ -16,5 +16,10 @@ namespace SoftwareEngineeringProject
         {
             InitializeComponent();
         }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+            //whoops
+        }
     }
 }
