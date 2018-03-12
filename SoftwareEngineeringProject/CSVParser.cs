7db33b60b524c3270ca6d2d56e1cf31dff69e573
Seanboyy <sbamford12@gmail.com>
2018-03-12 17:39:57 -0400

    Added Schedule Object

--- SoftwareEngineeringProject/CSVParser.cs
+++ SoftwareEngineeringProject/CSVParser.cs
@@ -21,22 +21,29 @@ namespace SoftwareEngineeringProject
             //add classes to the list of classes
             for(int i = 0; i < lineCount - 1; ++i)
             {
-                //split the line based on the ,
+                //split the line based on ,
                 string[] tokens = lines[i + 1].Split(',');
                 //use the constructor
                 classes[i] = new Class(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6], tokens[7], Convert.ToInt32(tokens[8]), Convert.ToInt32(tokens[9]));
             }
+            //start grabbing prerequisites
             for(int i = 0; i < lineCount2 - 1; ++i)
             {
+                //split the line based on ,
                 string[] tokens = lines2[i + 1].Split(',');
+                //seach through the list of classes
                 for(int j = 0; j < classes.Length; ++j)
                 {
+                    //to find the class associated with the course code
                     if(classes[j].courseCode == tokens[0])
                     {
+                        //searhc through the list of classes
                         for(int k = 0; k < classes.Length; ++k)
                         {
+                            //to find the class associated with the prerequisite's course code
                             if(classes[k].courseCode == tokens[1])
                             {
+                                //add the class to the list of classes
                                 classes[j].prerequisites.Add(classes[k]);
                             }
                         }
---
+++ SoftwareEngineeringProject/Schedule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineeringProject
+{
+    class Schedule
+    {
+        public List<Class> classes;
+
+        public Schedule() { }
+
+        public void AddClass(Class _class)
+        {
+            classes.Add(_class);
+        }
+
+        public void RemoveClass(Class _class)
+        {
+            classes.Remove(_class);
+        }
+    }
+}
