ae0bea914615a59b2d5323400bc41c34253bc759
Amy Lang <LangAC1@gcc.edu>
2018-04-19 15:52:41 -0400

    Added Lab_Lecture class - to find corresponding sections

---
+++ SoftwareEngineeringProject/Lab_Lecture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineeringProject
+{
+    public class Lab_Lecture
+    {
+        //find corresponding lab
+        public List<Course> Find_Labs(string courseCode)
+        {
+            List<Course> labList = new List<Course>();
+
+            return labList;
+        }
+        //find corresponding lecture
+        public List<Course> Find_Lectures(string courseCode)
+        {
+            List<Course> lectureList = new List<Course>();
+
+            return lectureList;
+        }
+    }
+}
