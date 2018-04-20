5435311f20aced77ef1413ed2487814a9c2d52ef
Amy Lang <LangAC1@gcc.edu>
2018-04-20 12:50:28 -0400

    Untested finding Lecture function

--- SoftwareEngineeringProject/Lab_Lecture.cs
+++ SoftwareEngineeringProject/Lab_Lecture.cs
@@ -9,16 +9,36 @@ namespace SoftwareEngineeringProject
     public class Lab_Lecture
     {
         //find corresponding lab
-        public List<Course> Find_Labs(string courseCode)
+        public List<Course> Find_Lectures(Course added_course, Course[] COURSE_LIST)
         {
             List<Course> labList = new List<Course>();
-
+            //determine if courseCode is a lab time
+            if(added_course.longTitle.Contains("Laboratory"))
+            {
+                //parse courseCode on " "
+                string[] tempCode = added_course.courseCode.Split(' ');
+                if(tempCode.Length >1)
+                {
+                    string posLect = tempCode[0] + tempCode[1];
+                    foreach(Course course in COURSE_LIST)
+                    {
+                        if (course.courseCode.Contains(posLect))
+                        {
+                            labList.Add(course);
+                        }
+                    }
+                }
+            }
+             
             return labList;
         }
         //find corresponding lecture
-        public List<Course> Find_Lectures(string courseCode)
+        public List<Course> Find_Labs(Course added_course, Course[] COURSE_LIST)
         {
             List<Course> lectureList = new List<Course>();
+
+            //determine if there is a corresponding lab for the class
+
 
             return lectureList;
         }
