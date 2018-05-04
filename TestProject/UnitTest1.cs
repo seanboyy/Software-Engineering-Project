7615bea22f9834eca569462333e161d7f6b550ee
BuildTools <sbamford12@gmail.com>
2018-05-04 15:01:15 -0400

    Added Testing Project

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoftwareEngineeringProject;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Lab_Lecture LL = new Lab_Lecture();
+            var courses = LL.Find_Labs(null, Form1.Instance.COURSE_LIST);
+            for(int i = 0; i < courses.Count; ++i)
+            {
+
+            }
+        }
+    }
+}
